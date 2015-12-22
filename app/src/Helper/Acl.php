bb9c18a2b016bb9d74abb5c44d9f5cc027eedc9c
Dwi Agus <spydersonics@gmail.com>
2015-12-23 00:09:09 +0700

    add acl lib

--- app/src/Helper/Acl.php
+++ app/src/Helper/Acl.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Helper;
-use \Slim\Http\Response as Response;
 use \App\Model\User;
 use \App\Model\UserPermission;
 /**
@@ -9,12 +8,10 @@ use \App\Model\UserPermission;
 class Acl
 {
 	private $session;
-	private $response;
 
 	public function __construct()
 	{
 		$this->session = new \App\Helper\Session;
-		$this->response = new Response;
 	}
 
 	public function profile()
